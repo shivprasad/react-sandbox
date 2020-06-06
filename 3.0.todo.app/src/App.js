9a90cfad61e367cbf2663a659e4db96e7f561b2c
Shiv Bade <shiv@shiv-mac-mini.local>
2020-06-05 21:39:26 -0700

    handle change for tasks

--- 3.0.todo.app/src/App.js
+++ 3.0.todo.app/src/App.js
@@ -9,11 +9,29 @@ class App extends React.Component {
     this.state = {
       todos : todoData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(taskId){
+    this.setState((prevState)=>{
+        let newToDo = prevState.todos.map((item)=>{
+          if(item.id === taskId) {
+            return {
+              ...item,
+              completed : !item.completed
+            }
+          }
+          return item
+        })
+        return {
+          todos: newToDo
+        }
+    })
   }
 
   render() {
     const data = this.state.todos.map( t => (
-      <TodoItem key={t.id} item={t} />
+      <TodoItem key={t.id} item={t}  handleChange={this.handleChange}/>
     ))
     return (
       <div className="todo-list">
--- 3.0.todo.app/src/TodoItem.js
+++ 3.0.todo.app/src/TodoItem.js
@@ -3,11 +3,13 @@ import { render } from "react-dom"
 
 class ToDoItem extends React.Component {
 
+
+
     render() {
         console.log(this.props)
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={this.props.item.completed} onChange={()=>{ console.log("wow...!!!")}} />
+                <input type="checkbox" checked={this.props.item.completed} onChange={()=>{ this.props.handleChange(this.props.item.id) }} />
                 <p>{this.props.item.task}</p>
             </div>
         )

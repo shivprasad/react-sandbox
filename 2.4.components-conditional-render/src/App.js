1184d0eb5aeee8f45886f42292f014601a9871c1
shivprasad <shivprasad@gmail.com>
2020-06-08 12:33:41 -0700

    conditional render

--- 2.4.components-conditional-render/src/App.js
+++ 2.4.components-conditional-render/src/App.js
@@ -1,34 +1,58 @@
 import React from "react"
-import MyInfo from "./MyInfo"
+//import MyInfo from "./MyInfo"
 
 class App extends React.Component {
 
   constructor(){
     super()
     this.state = {
-      isLoading: true
+      isLoggedIn: true
     }
+    this.logout = this.logout.bind(this)
+    this.login = this.login.bind(this)
   }
 
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({
-        isLoading : false
-      })
-    },1500)
+  logout(){
+    console.log("Logout")
+    this.setState({
+      isLoggedIn : false
+    })
+  }
+
+  login(){
+    console.log("Login")
+    this.setState({
+      isLoggedIn : true
+    })
   }
 
   render(){
     return (
         <div>
-           { this.state.isLoading ?
-            <h1>Loading...</h1> :
-            <MyInfo/>
-           } 
+            <div>
+              { this.state.isLoggedIn ?
+                <h1>Logged In</h1> :
+                <h1>Logged Out</h1> 
+              } 
+
+            </div>
+            <div>
+              <LogInOutButton isLoggedIn={this.state.isLoggedIn} onClickHandler={this.state.isLoggedIn ? this.logout : this.login}/> 
+            </div>
         </div>
     )
   }
 
 }
+
+class LogInOutButton extends React.Component {
+  render(){
+    return (
+      
+      <button onClick={()=>{ this.props.onClickHandler()}}>{this.props.isLoggedIn ? "Logout" : "Login"}</button> 
+    )
+  }
+}
+
 
 export default App
--- 2.4.components-conditional-render/src/index.js
+++ 2.4.components-conditional-render/src/index.js
@@ -2,6 +2,19 @@ import React from "react"
 import ReactDom from "react-dom"
 import App from "./App.js" //.js is optional
 
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+
+
+
 ReactDom.render(
   <App/>,
   document.getElementById("root")
